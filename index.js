abd53f9e9907008e10c9439fff7ae5d0f0f5fd3a
Rafael <rafael.waltrick@fatec.sp.gov.br>
2022-06-07 21:56:25 -0300

    Parei no update

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ async function selectCustomers(){
     return rows;
 }
 
+//index.js
+(async () => {
+    const db = require("./Banco");
+    console.log('Começou!');
+    
+    console.log('INSERT INTO CLIENTES');
+    const result = await db.insertCustomer({nome: "Zé", idade: 18, estado: "SP"});
+    console.log(result);
+
+    console.log('SELECT * FROM CLIENTES');
+    const clientes = await db.selectCustomers();
+    console.log(clientes);
+})();
+
 
 (async () => {
     const banco = require("./Banco");
